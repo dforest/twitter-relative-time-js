8d53a1ac13c6081fd2d5517a3a38ccae877124b9
Keita Mori <keita011@gmail.com>
2013-02-23 15:52:14 +0900

    released version 0.1.0

--- README.md
+++ README.md
@@ -1,4 +1,39 @@
-twitter-relative-time-js
-========================
+# twitter.relative.time.js
 
 Convert Date to relative time the twitter display requirements with javascript
+
+[Twitter Display Requirements](https://dev.twitter.com/terms/display-requirements)
+
+## Usage
+
+When using the plugin, you must first include `twitter.relative.time.js`.
+
+```js
+new Date().toTwitterRelativeTime()
+//=> 7s (for 7 seconds ago)
+//=> 13m (for 13 minutes ago)
+//=> 19h (for 19 hours ago)
+//=> 23 Jan (older than 24 hours)
+```
+
+When you want to display in Japanese, you should give the locale key `ja`.
+
+```js
+new Date().toTwitterRelativeTime('ja')
+//=> 7秒前 (for 7 seconds ago)
+//=> 13分前 (for 13 minutes ago)
+//=> 19時間前 (for 19 hours ago)
+//=> 1月23日 (older than 24 hours)
+```
+
+## LICENSE
+
+The MIT License
+
+Copyright (c) 2013 Keita Mori
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
---
+++ twitter.relative.time.js
@@ -0,0 +1,96 @@
+/**
+ * twitter.relative.time.js
+ * Version 0.1.0
+ * Copyright (c) 2013 Keita Mori
+ * https://github.com/dforest/twitter-relative-time-js
+ *
+ * Includes data.extentions.js
+ * Copyright (c) 2009 James F. Herdman
+ * https://github.com/jherdman/javascript-relative-time-helpers
+ *
+ * Released under the MIT License.
+ */
+ Date.prototype.toTwitterRelativeTime = (function() {
+  var locale = 'def';
+
+  var _ = function(locale_key) {
+    if (locale_key !== undefined && LOCALES.hasOwnProperty(locale_key)) { locale = locale_key;}
+    var now = new Date();
+    var delta = now - this;
+    delta = Math.abs(delta);
+
+    var units = t('now');
+    for (var key in CONVERSIONS) {
+      if (delta < CONVERSIONS[key])
+        break;
+      units = t(key);
+      delta = delta / CONVERSIONS[key];
+    }
+
+    if (delta/24 < 1) {
+      delta = Math.floor(delta);
+      if (units === t('now')) { delta = ''; }
+      else if (delta !== 1) { units += t('prural'); }
+      return [delta, units, units !== t('now') ? t('ago') : ''].join(t('spacing'));
+    }
+
+    return localize(this);
+
+  };
+
+  var t = function(key){
+    return LOCALES[locale][key];
+  };
+
+  var localize = function(date){
+    if(locale === 'ja'){
+      return [t('month')[date.getMonth()], date.getDate(), t('day')].join('');
+    }
+    return [date.getDate(), t('month')[date.getMonth()]].join(' ');
+  };
+
+  var CONVERSIONS = {
+    now: 1,         // ms    -> ms
+    second: 1000,   // ms    -> sec
+    minute: 60,     // sec   -> min
+    hour:   60      // min   -> hour
+  };
+  var LOCALES = {
+    def: {
+      prural: '',
+      spacing: '',
+      ago: '',
+      now: 'Now',
+      second: 's',
+      minute: 'm',
+      hour:   'h',
+      day: '',
+      month: ['Jan','Feb','Mar','Apr','May','June','July','Aug','Sept','Oct','Nov','Dec']
+    },
+    en: {
+      prural: 's',
+      spacing: ' ',
+      ago: 'ago',
+      now: 'Now',
+      second: 'second',
+      minute: 'minute',
+      hour:   'hour',
+      day: '',
+      month: ['January','February','March','April','May','June','July','August','September','Octover','November','December']
+    },
+    ja: {
+      prural: '',
+      spacing: '',
+      ago: '前',
+      now: '今',
+      second: '秒',
+      minute: '分',
+      hour:   '時間',
+      day: '日',
+      month: ['1月','2月','3月','4月','5月','6月','7月','8月','9月','10月','11月','12月']
+    }
+  }
+
+  return _;
+
+})();
